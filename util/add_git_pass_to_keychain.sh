5bc055b08e427953044a8776b27690a1f79ecc55
David Jensen <david.aka.dj@gmail.com>
2012-12-29 15:27:51 -0700

    added a utility script that makes it so git won't prompt for password and user every time you push to github

---
+++ util/add_git_pass_to_keychain.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#if you installed git then you should already have the osxkeychain helper but if you don't then you need to install it
+
+config --global credential.helper osxkeychain
--- util/make_github_repo.sh
+++ util/make_github_repo.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+#make a new github repo from CLI
+
 curl -i -u bonestorm -d '{"name" : "dbag", "auto_init": true}' https://api.github.com/user/repos
